aa98e43fa710d8b5863c40c341696b091c6e26ff
lxd7788 <964709569@qq.com>
2019-03-15 09:15:47 +0800

    11

--- src/main/java/com/lxd/controller/LoginController.java
+++ src/main/java/com/lxd/controller/LoginController.java
@@ -1,20 +1,28 @@
 package com.lxd.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lxd.model.Uu;
 import com.lxd.utils.EhcacheUtil;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
 	@RequestMapping("/login")
-	public ModelAndView login(){
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response){
 		//测试缓存
 		EhcacheUtil.getInstance().put("cacheTest", "tt", "fuck");
 		Object object = EhcacheUtil.getInstance().get("cacheTest","tt");
 		System.out.println("------------------"+object.toString());
+		Uu uu=new Uu();
+		uu.setAge("sadsaf");
+		uu.setName("sadasda");
+		request.getSession().setAttribute("user", uu);
 		return new ModelAndView("login");	
 	}
 }
--- src/main/webapp/WEB-INF/view/login.jsp
+++ src/main/webapp/WEB-INF/view/login.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%
+    	Object obj=session.getAttribute("user");
+    %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,4 +12,12 @@
 <body>
 sssssssss
 </body>
+<script type="text/javascript">
+window.onload=function(){
+	
+var mm='<%=obj%>';
+alert(mm);
+}
+
+</script>
 </html>
